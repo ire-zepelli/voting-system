4a68347a6b2eb1076dd8efc815de0e0d6f9023dc
Benedict <138974722+ire-zepelli@users.noreply.github.com>
2026-04-13 22:27:47 +0800

    added candidate poster

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Voting from "./pages/Voting";
+import CandidatePoster from "./pages/CandidatePoster";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/voting" element={<Voting />} />
+        <Route path="/candidate-poster" element={<CandidatePoster />} />
       </Routes>
     </>
   );
