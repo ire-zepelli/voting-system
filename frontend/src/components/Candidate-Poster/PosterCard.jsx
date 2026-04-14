2991d0c1c700310dbbffebdd6db45cecea58e068
Veejay Cuizon <veejaycuizon24@gmail.com>
2026-04-14 16:28:56 +0800

    Added updates for candidate poster

--- frontend/src/components/Candidate-Poster/PosterCard.jsx
+++ frontend/src/components/Candidate-Poster/PosterCard.jsx
@@ -3,31 +3,36 @@ import sampleImage from '../../assets/sample-poster.png'
 import CCS from '../../assets/uclmccs.png'
 import PSITS from '../../assets/uclmpsits.png'
 
-export default function PosterCard({ name, description, pdfLink }) {
+export default function PosterCard({ title, name, description, pdfLink }) {
     return (
         <div className=' ml-20 text-white p-4 w-11/12 rounded-lg shadow-lg bg-[#3B0B2E] '>
-            <div className='flex'>
-                <img src={CCS} alt="UCLM CCS Logo" className='w-16 h-auto' />
-                <img src={PSITS} alt="UCLM PSITS Logo" className='w-16 h-auto' /> 
+            <div className='flex justify-between items-center'>
+                <div className='flex'>
+                    <img src={CCS} alt="UCLM CCS Logo" className='w-16 h-auto' />
+                    <img src={PSITS} alt="UCLM PSITS Logo" className='w-16 h-auto' />
+                </div>
+                <p className='text-5xl'>{title}</p>
+                <div className='w-32' />
             </div>
-            
+
+
             <h2 className='-ml-6 p-5 text-7xl font-bold'>{name}</h2>
             <div className='flex m-w-full gap-15'>
-                <img src={sampleImage} alt={`${name}'s poster`} className='w-32 sm:w-48 md:w-64 lg:w-80 h-auto' />
+                <img src={sampleImage} alt={`${name}'s poster`} className='rounded-lg w-32 sm:w-48 md:w-64 lg:w-80 h-auto' />
                 <div className='flex flex-col'>
                     <p className='text-3xl'>{description}</p>
                     {pdfLink && (
-                        <a  
+                        <a
                             href={pdfLink}
                             target="_blank"
                             rel="noopener noreferrer"
-                            className='ml-auto self-start mt-auto px-4 py-2 bg-white text-[#3B0B2E] font-semibold rounded hover:bg-gray-200 transition'
-                            >
-                                View Credentials
-                            </a>
-                    )}    
+                            className='mb-5 ml-auto self-start mt-auto px-4 py-2 bg-white text-[#3B0B2E] font-semibold rounded hover:bg-gray-200 transition'
+                        >
+                            View Credentials
+                        </a>
+                    )}
                 </div>
-                
+
 
             </div>
         </div>
