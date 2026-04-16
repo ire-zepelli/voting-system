01b3ce66b49f08cc383ac267500cebb4ecf6a846
Veejay Cuizon <veejaycuizon24@gmail.com>
2026-04-16 14:09:06 +0800

    Fixed some stuff

--- frontend/src/components/Candidate-Poster/CandidateInfo.js
+++ frontend/src/components/Candidate-Poster/CandidateInfo.js
@@ -7,73 +7,73 @@ export const BEATScandidates = [
         title: "President",
         name: "CRISTA MONICA OSCAR",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/Oscar.pdf"
+        pdfLink: "../../pdf/Oscar.pdf"
     },
     {
         title: "Vice-President Internal",
         name: "DAN PIERRE O. POGOY",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/Pogoy.pdf"
+        pdfLink: "../../pdf/Pogoy.pdf"
     },
     {
         title: "Vice-President External",
         name: "RYAN PACUMIO",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/Pacumio.pdf"
+        pdfLink: "../../pdf/Pacumio.pdf"
     },
     {
         title: "Secretary",
         name: "MAY O. LAPEÑA",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/Lapeña.pdf"
+        pdfLink: "../../pdf/Lapeña.pdf"
     },
     {
         title: "Public Information Officer",
         name: "FIONA M. MONILAR",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/Monilar.pdf"
+        pdfLink: "../../pdf/Monilar.pdf"
     },
     {
         title: "Treasurer",
         name: "KEITH RAMISES S. LATONIO",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/Latonion.pdf"
+        pdfLink: "../../pdf/Latonio.pdf"
     },
     {
         title: "Chief of Creatives",
         name: "HARRY O. CONDE",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/Conde.pdf"
+        pdfLink: "../../pdf/Conde.pdf"
     },
     {
         title: "Auditor",
         name: "EMMANUEL FRANZ B. APAWAN",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/Apawan.pdf"
+        pdfLink: "../../pdf/Apawan.pdf"
     },
     {
         title: "Chief of Representative",
         name: "JHOVIEGEN N. CUYSONA",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/Cuysona.pdf"
+        pdfLink: "../../pdf/Cuysona.pdf"
     },
     {
         title: "Chief of Students Development",
         name: "AIMEE GAYLE B. COGAL",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/Cogal.pdf"
+        pdfLink: "../../pdf/Cogal.pdf"
     },
     {
         title: "Academic Representative",
         name: "ROSE ANNE R. RESURRECCION",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/Resurreccion.pdf"
+        pdfLink: "../../pdf/Resurreccion.pdf"
     },
     {
         title: "CARES Representative",
         name: "MARY GRACE E. PATALINGHUG",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/Patalinghug.pdf"
+        pdfLink: "../../pdf/Patalinghug.pdf"
     },
 
 ];
@@ -83,66 +83,66 @@ export const PEAKcandidates = [
         title: "President",
         name: "DARREN VILLANUEVA",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/VILLANUEVA.pdf"
+        pdfLink: "../../pdf/VILLANUEVA.pdf"
     },
     {
         title: "Vice-President Internal",
         name: "ALTHEIA DANO",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/DAÑO.pdf"
+        pdfLink: "../../pdf/DAÑO.pdf"
     },
     {
         title: "Vice-President External",
         name: "KANE HUXLEY BOOK",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/BOOK.pdf"
+        pdfLink: "../../pdf/BOOK.pdf"
     },
     {
         title: "Secretary",
         name: "DIANE MENDOZA",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/MENDOZA.pdf"
+        pdfLink: "../../pdf/MENDOZA.pdf"
     },
     {
         title: "Public Information Officer",
         name: "JEA MARY TRIXY MAGALLAND",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/MAGALLANO.pdf"
+        pdfLink: "../../pdf/MAGALLANO.pdf"
     },
     {
         title: "Treasurer",
         name: "HANNY JANE ENRIQUEZ",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/ENRIQUEZ.pdf"
+        pdfLink: "../../pdf/ENRIQUEZ.pdf"
     },
     {
         title: "Chief of Creatives",
         name: "TRISTHAN MARK VINCENT VILLAMOR",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/VILLAMOR.pdf"
+        pdfLink: "../../pdf/VILLAMOR.pdf"
     },
     {
         title: "Auditor",
         name: "MYKA ANGELA DUMAEL",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/DUMAEL.pdf"
+        pdfLink: "../../pdf/DUMAEL.pdf"
     },
     {
         title: "Chief of Representative",
         name: "ABIJAH SHEN REGADO",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/REGADO.pdf"
+        pdfLink: "../../pdf/REGADO.pdf"
     },
     {
         title: "Chief of Students Development",
         name: "JEOFF ANDREW DEMECILLO",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/Demecillo.pdf"
+        pdfLink: "../../pdf/Demecillo.pdf"
     },
     {
         title: "CARES Representative",
         name: "NATHANIEL ORNOPIA",
         description: defaultDescription,
-        pdfLink: "../../public/pdf/ORNOP.pdf"
+        pdfLink: "../../pdf/ORNOPIA.pdf"
     },
 ];
